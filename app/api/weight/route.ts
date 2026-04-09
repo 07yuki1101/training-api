67de7719d6a5079e99c5373fc7c9e112343645de
Yuuki Suzuki <07yuki1101@gmail.com>
2026-04-09 22:20:44 +0900

    add options

--- app/api/weight/route.ts
+++ app/api/weight/route.ts
@@ -23,3 +23,13 @@ export async function POST(req: Request) {
       );
     }
   }
+  export async function OPTIONS() {
+    return new Response(null, {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    });
+  }
