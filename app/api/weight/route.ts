f3f80763470d8fa0bd882e39508134b3089107c3
Yuuki Suzuki <07yuki1101@gmail.com>
2026-04-09 21:57:35 +0900

    add POST api

--- app/api/weight/route.ts
+++ app/api/weight/route.ts
@@ -3,3 +3,23 @@ import { NextResponse } from "next/server";
 export async function GET() {
   return NextResponse.json({ message: "API OK" });
 }
+
+export async function POST(req: Request) {
+    try {
+      const body = await req.json();
+  
+      console.log("受け取った:", body);
+  
+      return NextResponse.json({
+        success: true,
+        body,
+      });
+    } catch (error) {
+      console.error(error);
+  
+      return NextResponse.json(
+        { success: false },
+        { status: 500 }
+      );
+    }
+  }
